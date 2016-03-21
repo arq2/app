a97f0e86ead6f59c4596648b96a49eb9f2b43d92
Nahuel Garbezza <n.garbezza@gmail.com>
2016-03-21 20:12:32 -0300

    configuración de logging con logentries

--- src/main/java/ar/edu/unq/arq2/CuentasResource.java
+++ src/main/java/ar/edu/unq/arq2/CuentasResource.java
@@ -1,5 +1,8 @@
 package ar.edu.unq.arq2;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,10 +11,13 @@ import javax.ws.rs.core.MediaType;
 @Path("cuentas")
 public class CuentasResource {
 
+    private static Logger log = LogManager.getRootLogger();
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("ultima")
     public CuentaBean ultimaCuenta() {
+        log.info("en /cuentas/ultima");
         return new CuentaBean(456, "cosme fulanito", 100L);
     }
 }
--- src/main/java/ar/edu/unq/arq2/MyResource.java
+++ src/main/java/ar/edu/unq/arq2/MyResource.java
@@ -1,5 +1,8 @@
 package ar.edu.unq.arq2;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,6 +14,8 @@ import javax.ws.rs.core.MediaType;
 @Path("myresource")
 public class MyResource {
 
+    private static Logger log = LogManager.getRootLogger();
+
     /**
      * Method handling HTTP GET requests. The returned object will be sent
      * to the client as "text/plain" media type.
@@ -20,6 +25,7 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
+        log.info("en /myresource");
         return "Hello, Heroku!";
     }
 }
